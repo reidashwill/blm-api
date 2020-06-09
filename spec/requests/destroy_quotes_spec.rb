3a047aca2c73e1ba9808968721fdd92a0462e8e3
Reid Ashwill <reidashwill@gmail.com>
2020-06-09 15:20:03 -0700

    attempted to test following documentation, did not work, creating new branch before adding code

--- spec/requests/destroy_quotes_spec.rb
+++ spec/requests/destroy_quotes_spec.rb
@@ -1,13 +1,22 @@
 require 'rails_helper'
 require 'jwt'
 
-describe "delete quote route" do
+describe "delete quote route", :type => :api do
     # before(:each) {
     #   curl -H {"'Content-Type: application/json' -X POST -d '{'email':'test@test.com','password':'password'}' http://localhost:3000/authenticate"
     #   }
     # }
 
+   
+  #  'eyJ1c2VyX2lkIjoxLCJleHAiOjE1OTE4MTI0MzB9uNbvTpaQusxb6YdHtBgCuZ9KWolfH4eOtCUuQOo4jaE'
+  #      "Authorization"
     
+    before do 
+      token = JWT.encode({user:User.first.id}, 
+      ENV["AUTH_SECRET"], "HS256"
+      header "Authorization", "Bearer #{token}")
+    end
+
   it 'will delete a quote and return correct message' do
     test_quote = Quote.create!({author: 'test author', content: "test content"})
     delete "/quotes/#{test_quote.id}"
@@ -23,5 +32,5 @@ describe "delete quote route" do
   #   expect(JSON.parse(response.body)['message']).to eq ("Couldn't find Quote with 'id'=#{test_quote.id}")
   # end
 end
-
+# get '/my/path', nil, {'HTTP_ACCEPT' => "application/json"}
 # curl -H "Auth1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE1OTE4MTI0MzB9.uNbvTpaQusxb6YdHtBgCuZ9KWolfH4eOtCUuQOo4jaE" http://localhost:3000/quotes
